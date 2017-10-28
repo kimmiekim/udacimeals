79b3d37274ab2e15f33cc6e3f14ae46fe3594086
Erin Kim <kim.erin.kim@gmail.com>
2017-10-28 12:14:57 -0400

    added mapDispatchToProps

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar} from '../actions'
 
 class App extends Component {
+  doThings = () => {
+    this.props.selectRecipe({})
+  }
   render(){
     console.log('Props', this.props)
     return(
@@ -32,32 +36,16 @@ function mapStateToProps(calendar) {
         console.log("reduced meals to:", meals);
         return meals
       }, {})
-    }}),
+    }})
   }
+}
 
-  // return {
-  //   calendar: dayOrder.map((day)=> ({
-  //     day,
-  //     meals: Object.keys(calendar[day]).reduce((meals, meal)=> {
-  //       meals[meal] = calendar[day][meal]
-  //         ? calendar[day][meal]
-  //         :null
-  //         return meals
-  //     }, {})
-  //   }))
-  // }
-
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data)),
+  }
 }
 
 
-export default connect(mapStateToProps)(App)
-
-//
-// day,
-//  meals: Object.keys(calendar[day]).reduce((meals, meal) => {
-//     meals[meal] = calendar[day][meal]
-//       ? calendar[day][meal]
-//       : null
-//
-//     return meals
-// }, {})
+export default connect(mapStateToProps, mapDispatchToProps)(App)
