0e26c78c9238931deb3f2c9f943809a1ab027695
Erin Kim <kim.erin.kim@gmail.com>
2017-10-28 16:30:54 -0400

    added shopping list component and added modals show ingredients. search function is not able to make an api call to edamam, getting no cors allowed error

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { addRecipe, removeFromCalendar} from '../actions'
+import { addRecipe, removeFromCalendar } from '../actions'
 import { capitalize } from '../utils/helpers'
 import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o'
 import Modal from 'react-modal'
@@ -8,6 +8,7 @@ import ArrowRightIcon from 'react-icons/lib/fa/arrow-circle-right'
 import Loading from 'react-loading'
 import { fetchRecipes } from '../utils/api'
 import FoodList from './FoodList'
+import ShoppingList from './ShoppingList'
 
 class App extends Component {
   state = {
@@ -15,17 +16,16 @@ class App extends Component {
     meal: null,
     day: null,
     food: null,
-    loadingFood: true,
+    ingredientsModalOpen: false,
+    loadingFood: false,
   }
-
-  openFoodModal = ({meal, day}) => {
+  openFoodModal = ({ meal, day }) => {
     this.setState(() => ({
       foodModalOpen: true,
       meal,
       day,
     }))
   }
-
   closeFoodModal = () => {
     this.setState(() => ({
       foodModalOpen: false,
@@ -44,23 +44,45 @@ class App extends Component {
     this.setState(() => ({ loadingFood: true }))
 
     fetchRecipes(this.input.value)
-      .then((food) => this.setState(()=> ({
+      .then((food) => this.setState(() => ({
         food,
         loadingFood: false,
       })))
   }
+  openIngredientsModal = () => this.setState(() => ({ ingredientsModalOpen: true }))
+  closeIngredientsModal = () => this.setState(() => ({ ingredientsModalOpen: false }))
+  generateShoppingList = () => {
+    return this.props.calendar.reduce((result, { meals }) => {
+      const { breakfast, lunch, dinner } = meals
 
+      breakfast && result.push(breakfast)
+      lunch && result.push(lunch)
+      dinner && result.push(dinner)
 
-
-  render(){
-    const { foodModalOpen, loadingFood, food } = this.state
-    const { calendar, remove, selectRecipe } = this.props
+      return result
+    }, [])
+    .reduce((ings, { ingredientLines }) => ings.concat(ingredientLines), [])
+  }
+  render() {
+    const { foodModalOpen, loadingFood, food, ingredientsModalOpen } = this.state
+    const { calendar, selectRecipe, remove } = this.props
     const mealOrder = ['breakfast', 'lunch', 'dinner']
 
-    return(
+
+    return (
       <div className='container'>
-        <ul className = 'meal-types'>
-          {mealOrder.map( (mealType) => (
+
+        <div className='nav'>
+          <h1 className='header'>UdaciMeals</h1>
+          <button
+            className='shopping-list'
+            onClick={this.openIngredientsModal}>
+              Shopping List
+          </button>
+        </div>
+
+        <ul className='meal-types'>
+          {mealOrder.map((mealType) => (
             <li key={mealType} className='subheader'>
               {capitalize(mealType)}
             </li>
@@ -69,20 +91,20 @@ class App extends Component {
 
         <div className='calendar'>
           <div className='days'>
-            {calendar.map(({day}) => <h3 key={day} className='subheader'>{capitalize(day)}</h3>)}
+            {calendar.map(({ day }) => <h3 key={day} className='subheader'>{capitalize(day)}</h3>)}
           </div>
-          <div className = 'icon-grid'>
+          <div className='icon-grid'>
             {calendar.map(({ day, meals }) => (
               <ul key={day}>
-                {mealOrder.map((meal)=> (
+                {mealOrder.map((meal) => (
                   <li key={meal} className='meal'>
                     {meals[meal]
                       ? <div className='food-item'>
-                        <img src={meals[meal].image} alt={meals[meal].label} />
+                          <img src={meals[meal].image} alt={meals[meal].label}/>
                           <button onClick={() => remove({meal, day})}>Clear</button>
                         </div>
-                      : <button onClick = {() => this.openFoodModal({ meal, day })} className='icon-btn'>
-                            <CalendarIcon size={30} />
+                      : <button onClick={() => this.openFoodModal({meal, day})} className='icon-btn'>
+                          <CalendarIcon size={30}/>
                         </button>}
                   </li>
                 ))}
@@ -101,7 +123,7 @@ class App extends Component {
           <div>
             {loadingFood === true
               ? <Loading delay={200} type='spin' color='#222' className='loading' />
-              : <div className='seach-container'>
+              : <div className='search-container'>
                   <h3 className='subheader'>
                     Find a meal for {capitalize(this.state.day)} {this.state.meal}.
                   </h3>
@@ -121,15 +143,23 @@ class App extends Component {
                   {food !== null && (
                     <FoodList
                       food={food}
-                      onSelect = {(recipe) => {
+                      onSelect={(recipe) => {
                         selectRecipe({ recipe, day: this.state.day, meal: this.state.meal })
                         this.closeFoodModal()
-                      }} />
-                  )}
-                </div>
-              }
+                      }}
+                    />)}
+                </div>}
           </div>
+        </Modal>
 
+        <Modal
+          className='modal'
+          overlayClassName='overlay'
+          isOpen={ingredientsModalOpen}
+          onRequestClose={this.closeIngredientsModal}
+          contentLabel='Modal'
+        >
+          {ingredientsModalOpen && <ShoppingList list={this.generateShoppingList()}/>}
         </Modal>
 
       </div>
@@ -137,36 +167,31 @@ class App extends Component {
   }
 }
 
-//inside mapStateToProps function we are reformatting the js obj
-//into arrays
-//so react and work with the array of data
-//with redux, it's more natural to have data structure in obj format
-
-function mapStateToProps({calendar, food}) {
+function mapStateToProps ({ food, calendar }) {
   const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 
   return {
-    calendar: dayOrder.map((day) => {
-      console.log("for ",day);
-      return {
+    calendar: dayOrder.map((day) => ({
       day,
       meals: Object.keys(calendar[day]).reduce((meals, meal) => {
         meals[meal] = calendar[day][meal]
           ? food[calendar[day][meal]]
           : null
-        console.log("reduced meals to:", meals);
+
         return meals
       }, {})
-    }})
+    })),
   }
 }
 
 function mapDispatchToProps (dispatch) {
   return {
     selectRecipe: (data) => dispatch(addRecipe(data)),
-    remove: (data) => dispatch(removeFromCalendar(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
   }
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
